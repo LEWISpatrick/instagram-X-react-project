7016f6e03199aef32b244c947ef36a65883c33f3
LEWISpatrick <133691910+LEWISpatrick@users.noreply.github.com>
2024-03-04 13:07:23 -0300

    Starting to add project files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css';
+// Pages Import
+import {Main} from './pages/main/main'
+import {Login} from './pages/login'
+import { CreatePost } from './pages/create-post/create-post';
+import { Profile } from './pages/profile/profile';
+import {NavBar} from './components/navbar'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/createpost' element={<CreatePost />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
